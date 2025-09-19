c2fc8b248146dc03cf212abe8b7461bfa29783ea
Sachin <sachinjadhav@Sachins-MBP.attlocal.net>
2025-09-19 11:28:25 -0500

    Update next config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-// next.config.js
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export", // enables static export
+//   images: { unoptimized: true }, // since Next Image optimizer doesn’t work with export
+//   basePath: "/portfolio", // your repo name
+//   assetPrefix: "/portfolio/",
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static export
-  images: { unoptimized: true }, // since Next Image optimizer doesn’t work with export
-  basePath: "/portfolio", // your repo name
-  assetPrefix: "/portfolio/",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
